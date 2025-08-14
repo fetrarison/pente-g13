c2832c750c081476290343e34c20e1429e6b52ed
fetrarison <fetrarison5@gmail.com>
2025-08-14 14:28:47 +0300

    chore: format files

--- src/main/java/com/examen/demo/io/CsvPointReader.java
+++ src/main/java/com/examen/demo/io/CsvPointReader.java
@@ -1,7 +1,6 @@
 package com.examen.demo.io;
 
 import com.examen.demo.geom.Point3D;
-
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
@@ -10,22 +9,22 @@ import java.util.List;
 
 public class CsvPointReader {
 
-    public static List<Point3D> readCsvPoints(String path){
-        List<Point3D> pts = new ArrayList<>();
-        try {
-            for (String raw : Files.readAllLines(Paths.get(path))) {
-                String line = raw.trim();
-                if (line.isEmpty() || line.startsWith("#") || line.startsWith("x")) continue;
-                String[] t = line.split("\\s*,\\s*");
-                if (t.length < 3) continue;
-                double x = Double.parseDouble(t[0]);
-                double y = Double.parseDouble(t[1]);
-                double z = Double.parseDouble(t[2]);
-                pts.add(new Point3D(x, y, z));
-            }
-        } catch (IOException e) {
-            throw new RuntimeException("Erreur lecture CSV: " + e.getMessage(), e);
-        }
-        return pts;
+  public static List<Point3D> readCsvPoints(String path) {
+    List<Point3D> pts = new ArrayList<>();
+    try {
+      for (String raw : Files.readAllLines(Paths.get(path))) {
+        String line = raw.trim();
+        if (line.isEmpty() || line.startsWith("#") || line.startsWith("x")) continue;
+        String[] t = line.split("\\s*,\\s*");
+        if (t.length < 3) continue;
+        double x = Double.parseDouble(t[0]);
+        double y = Double.parseDouble(t[1]);
+        double z = Double.parseDouble(t[2]);
+        pts.add(new Point3D(x, y, z));
+      }
+    } catch (IOException e) {
+      throw new RuntimeException("Erreur lecture CSV: " + e.getMessage(), e);
     }
+    return pts;
+  }
 }
